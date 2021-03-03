0ac2145cd138845caa5ef8ae9501be9efd337050
Ted Bowman <ted@tedbow.com>
2021-03-03 10:17:30 -0500

    allow pushing to other remotes with checks

--- pre-push.php
+++ pre-push.php
@@ -1,5 +1,15 @@
 #!/usr/bin/env php
 <?php
+$remote = $argv[1];
+if (!$remote) {
+    print "⚠️Could not determine remote. Use the git-push.php file.\n";
+    exit(1);
+}
+// Allow pushing to my backup on github without checks.
+if ($remote === 'github') {
+    exit(0);
+}
+
 if (!file_exists('.pre-push')) {
     print "💁🏼‍♂️Use the git-push.php file.\n";
     exit(1);
