93c6eeb2628e2d7e3f9727383e2f49e6d139efa0
Ted Bowman <ted@tedbow.com>
2021-03-09 08:31:01 -0500

    delete phptest.php

--- phptest.php
+++
@@ -1,9 +0,0 @@
-#! /usr/local/opt/php@7.3/bin/php
-<?php
-print_r(getopt(null, ["h:"]));
-exit;
-print system("vendor/bin/phpunit --configuration core core/modules/update/tests/src/Unit/ProjectCoreCompatibilityTest.php");
-exit();
-print_r($argv);
-
-print_r(getopt(null, ["name:"]));
--- src/Command/Checkers/PHPUnitChecker.php
+++ src/Command/Checkers/PHPUnitChecker.php
@@ -30,6 +30,7 @@ class PHPUnitChecker extends CheckerBase
     }
 
 
+
     /**
      * @inheritDoc
      */
