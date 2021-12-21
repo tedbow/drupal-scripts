24dfdf453585446b4d1924ed14ff61de1637806d
Ted Bowman <ted@tedbow.com>
2021-12-21 12:33:42 -0500

    comments

--- src/ScriptApplication.php
+++ src/ScriptApplication.php
@@ -10,7 +10,13 @@ class ScriptApplication extends Application
     protected $xdebugConfirmed = false;
 
     /**
+     * Checks if xdebug option has already been confirmed.
+     *
+     * This helps when calling 1 command from another.
+     *
      * @return bool
+     *
+     * @see \TedbowDrupalScripts\Command\CommandBase::confirmXedbug()
      */
     public function isXdebugConfirmed(): bool
     {
@@ -18,7 +24,11 @@ class ScriptApplication extends Application
     }
 
     /**
+     * Sets xdebug option confirmed.
+     *
      * @param bool $xdebugConfirmed
+     *
+     * @see \TedbowDrupalScripts\Command\CommandBase::confirmXedbug()
      */
     public function setXdebugConfirmed(bool $xdebugConfirmed = true): void
     {
