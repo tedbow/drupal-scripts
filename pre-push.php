d13f735d7c8b01eae3a6668d582e195d315ed139
Ted Bowman <ted@tedbow.com>
2021-12-21 15:14:16 -0500

    make ignored remotes a setting

--- pre-push.php
+++ pre-push.php
@@ -1,12 +1,25 @@
 #!/usr/bin/env php
 <?php
+/**
+ * For this to work you must make a link to this file from
+ * .git/hooks/pre-push
+ *
+ * This will stop 'git push' from working directly unless
+ * 1. The remote name is specifed in the  'remotes_ignore' setting.
+ * 2. A '.pre-push' file is present.
+ */
+
+use TedbowDrupalScripts\Settings;
+
+require_once __DIR__ . '/vendor/autoload.php';
 $remote = $argv[1];
 if (!$remote) {
     print "⚠️Could not determine remote. Use the git-push.php file.\n";
     exit(1);
 }
-// Allow pushing to my backup on github without checks.
-if ($remote === 'github') {
+// Allow pushing to some remotes for backup without checks.
+$ignore_remotes = Settings::getSetting('remotes_ignore');
+if (in_array($remote, $ignore_remotes)) {
     exit(0);
 }
 
